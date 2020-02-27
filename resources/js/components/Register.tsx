c6f8e0aea01a6031dc5703b2210997d088270f12
Kanade <andy_wang0625@hotmail.com>
2020-02-27 06:16:21 -0500

    Updated Register Component

--- resources/js/components/Register.tsx
+++ resources/js/components/Register.tsx
@@ -1,10 +1,11 @@
-import { Component } from "react";
+import { Component, HtmlHTMLAttributes } from "react";
 import React from "react";
 import { TextField, Container, Grid, createStyles, makeStyles, Theme, withStyles, InputAdornment, Button } from "@material-ui/core";
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import EmailRoundedIcon from '@material-ui/icons/EmailRounded';
 import LockRoundedIcon from '@material-ui/icons/LockRounded';
 import FormErrorMessage from './FormErrorMessage';
+import axios from "axios";
 
 interface RegState{
     userName?: string;
@@ -19,7 +20,7 @@ const styles = (theme:Theme) => ({
         margin: theme.spacing(1),
         width: "100%",
     }
-})
+});
 
 class Register extends Component<any, RegState>{
     constructor(props:any){
@@ -32,6 +33,7 @@ class Register extends Component<any, RegState>{
             errorMessage:undefined,
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e: React.ChangeEvent<HTMLInputElement>){
@@ -46,17 +48,29 @@ class Register extends Component<any, RegState>{
         });
     }
 
+    handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        if(this.state.userPasswd1==this.state.userPasswd2){
+            const {userEmail, userName, userPasswd1} = this.state;
+            console.log("form submitted");
+        }else{
+            console.log("Error");
+        }
+
+    }
+
     render(){
         const {classes} = this.props;
         return(
             <Container maxWidth="md">
-                <Grid container>
-                    <form>
+                <Grid container style={{marginTop: 20, alignItems:"center", display:"flex"}}>
+                    <form onSubmit={this.handleSubmit}>
                         <TextField
                             className={classes.margin}
                             id="userNameText"
                             name="userName"
                             label="User Name"
+                            placeholder="User Name"
                             onChange={this.handleChange}
                             value={this.state.userName}
                             InputProps={{
@@ -66,10 +80,11 @@ class Register extends Component<any, RegState>{
                                     </InputAdornment>
                                 ),
                             }}
-                            ></TextField>
+                            required></TextField>
                         <TextField
                             className={classes.margin}
                             id="userEmail"
+                            placeholder="Email"
                             name="userEmail"
                             label="Email"
                             onChange={this.handleChange}
@@ -80,11 +95,13 @@ class Register extends Component<any, RegState>{
                                         <EmailRoundedIcon></EmailRoundedIcon>
                                     </InputAdornment>
                                 )
-                            }}></TextField>
+                            }}
+                            required></TextField>
                         <TextField
                             className={classes.margin}
                             id="userPasswd1"
                             name="userPasswd1"
+                            placeholder="Password"
                             label="Password"
                             value={this.state.userPasswd1}
                             onChange={this.handleChange}
@@ -94,11 +111,13 @@ class Register extends Component<any, RegState>{
                                     <LockRoundedIcon></LockRoundedIcon>
                                     </InputAdornment>
                                 )
-                            }}></TextField>
+                            }}
+                            required></TextField>
                             <TextField
                             className={classes.margin}
                             id="userPasswd2"
                             name="userPasswd2"
+                            placeholder="Password Verification"
                             label="Password Verification"
                             value={this.state.userPasswd2}
                             onChange={this.handleChange}
@@ -108,14 +127,15 @@ class Register extends Component<any, RegState>{
                                         <LockRoundedIcon></LockRoundedIcon>
                                     </InputAdornment>
                                 )
-                            }}></TextField>
+                            }}
+                            required></TextField>
                         {this.state.errorMessage !== undefined ?(
                             <FormErrorMessage theMessage={this.state.errorMessage}></FormErrorMessage>
                         ):null}
                         <Button className={classes.margin} type="submit" variant="contained" color="primary">Register</Button>
                     </form>
 
-                    
+
                 </Grid>
             </Container>
         );
