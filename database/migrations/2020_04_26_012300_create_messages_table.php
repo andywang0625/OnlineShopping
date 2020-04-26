2203562ac3529799cb285cf17828b9e678bd97fd
Kanade <andy_wang0625@hotmail.com>
2020-04-25 21:57:11 -0400

    Create Migrate of the Messages Table

---
+++ database/migrations/2020_04_26_012300_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("content")->charset("utf8")->nullale(false);
+            $table->bigInteger("senderid")->unsigned();
+            $table->bigInteger("targetid")->unsigned();
+            $table->foreign("senderid")->references("id")->on("users")->onDelete("cascade");
+            $table->foreign("targetid")->references("id")->on("users")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
