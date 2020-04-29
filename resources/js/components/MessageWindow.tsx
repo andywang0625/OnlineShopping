6c6e1a6bef8d2dbfec8f8bb8227dfc62e7de14d8
Kanade <andy_wang0625@hotmail.com>
2020-04-29 00:34:35 -0400

    Update MessageWindow.tsx

--- resources/js/components/MessageWindow.tsx
+++ resources/js/components/MessageWindow.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
+import axios from "axios";
+import Loading from './Loading';
 
 interface MessageWindowState{
     message?:string;
     messagesList?:string[];
+    isFetching:boolean;
 }
 
 class MessageWindow extends Component<any, MessageWindowState> {
@@ -13,14 +16,49 @@ class MessageWindow extends Component<any, MessageWindowState> {
         //console.log(this.props.location.targetid);
         this.state = {
             message:undefined,
+            messagesList:undefined,
+            isFetching:true,
         }
     }
+    componentDidMount(){
+        let messages:any[] = [];
+        axios({
+            method: 'get',
+            url: '/api/messages/sent',
+            params:{
+                token:this.props.token,
+            }
+        }).then((response)=>{
+            messages = response.data.messages;
+            axios({
+                method: "get",
+                url: '/api/messages/received',
+                params:{
+                    token:this.props.token,
+                }
+            }).then((response)=>{
+                messages = messages.concat(response.data.messages);
+                messages = messages.sort((a, b)=>{
+                    return a.created_at > b.created_at?1:-1;
+                });
+                this.setState({
+                    messagesList:messages,
+                    isFetching:false,
+                })
+            });
+        });
+    }
     render() {
-        return (
-            <div>
+        if(this.state.isFetching)
+            return (
+                <Loading></Loading>
+            );
+        else
+            return (
+                <div>
 
-            </div>
-        );
+                </div>
+            );
     }
 }
 
