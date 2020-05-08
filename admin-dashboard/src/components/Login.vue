4bc0384f99378a8f6ad5d9735a5f225d5bee013d
andywang0625 <andy_wang0625@hotmail.com>
2020-05-08 17:31:43 -0400

    create admin-dashboard

---
+++ admin-dashboard/src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="24"><el-input class="grid-content" placeholder="User Name" v-model="username"></el-input></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24"><el-input placeholder="Password" v-model="password" @change="handlePasswordChange" show-password></el-input></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <el-alert
+                    v-show="showError"
+                    title="Password Cannot be Empty"
+                    type="error"></el-alert>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Login",
+        data:()=>{
+            return {
+                username: "",
+                password: "",
+                showError: false,
+            };
+        },
+        methods:{
+            handlePasswordChange: function(){
+                if(this.password==""){
+                    this.showError=true;
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
